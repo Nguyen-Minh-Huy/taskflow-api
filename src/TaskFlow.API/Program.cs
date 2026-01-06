646d0c4b45f06d2db259bb8f251616def66ba048
mHuy11052003 <huy.2174802010934@vanlanguni.vn>
2026-01-06 21:51:12 +0700

    chore: modified Program.cs & launchSettings.json

--- src/TaskFlow.API/Program.cs
+++ src/TaskFlow.API/Program.cs
@@ -1,15 +1,31 @@
+﻿using Microsoft.OpenApi;
+
 var builder = WebApplication.CreateBuilder(args);
 
+// Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "TaskFlow API",
+        Version = "v1",
+        Description = "TaskFlow API documentation"
+    });
+});
 
 var app = builder.Build();
 
+// Enable Swagger UI only in Development
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "TaskFlow API V1");
+        c.RoutePrefix = string.Empty; // Swagger UI hiện ở root: http://localhost:5183/
+    });
 }
 
 app.UseHttpsRedirection();
